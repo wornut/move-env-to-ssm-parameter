bce65ea5d8783c31a5bcd0866768ae54843a777d
worasitipon <worasitipon.t@hotmail.com>
2021-08-04 12:46:56 +0700

    feat: :zap: add fetch all repo generator

--- src/get_env_from_bitbucket_pipeline/index.ts
+++ src/get_env_from_bitbucket_pipeline/index.ts
@@ -3,6 +3,16 @@ import { question, argv, fetch } from 'zx';
 import { RequestInit, Response } from 'node-fetch';
 import { get, isEqual } from 'lodash-es';
 import { log } from '../helper/logger';
+import { from } from 'rxjs';
+
+const BITBUCKET_BASE_URL = 'https://api.bitbucket.org/2.0';
+
+type Query = {
+    workspace: string;
+    project_key: string; // slug
+    repo_slug: string; // slug
+    deployment_id: string;
+};
 
 async function parseCliParameters() {
     const username: string = argv['username'] ?? (await question('Bitbucket username: '));
@@ -20,54 +30,81 @@ async function parseCliParameters() {
 
 function bbclient(username: string, apptoken: string) {
     const auth64 = Buffer.from([username, apptoken].join(':')).toString('base64');
-    const baseUrl = 'https://api.bitbucket.org/2.0';
     const headers = {
         Authorization: `Basic ${auth64}`,
         'Content-Type': 'application/json',
     };
 
-    return (path: string, option: RequestInit) => fetch(baseUrl + path, { headers, ...option });
+    return (path: string, option: RequestInit) => fetch(BITBUCKET_BASE_URL + path, { headers, ...option });
 }
 
-function waitResponse(res: Response) {
-    return res.json();
+function waitResponse<T>(req: Promise<Response>): Promise<T> {
+    return req.then((res) => res.json()) as unknown as Promise<T>;
 }
 
 async function selectWorkspace(client: ReturnType<typeof bbclient>) {
-    const { values: workspaceValues } = await client('/workspaces?fields=values.slug', { method: 'GET' }).then(
-        waitResponse,
-    );
+    const { values } = await waitResponse<{
+        values: {
+            slug: string;
+        }[];
+    }>(client('/workspaces?fields=values.slug', { method: 'GET' }));
 
     return question('Select a workspace (press TAB for hint): ', {
-        choices: Array.from(workspaceValues).map((v) => get(v, 'slug', '-')),
+        choices: Array.from(values).map((v) => get(v, 'slug', '-')),
     });
 }
 
-async function selectProject(client: ReturnType<typeof bbclient>, workspace: string) {
-    const { values: projectValues } = await client(`/workspaces/${workspace}/projects?fields=values.key,values.uuid`, {
-        method: 'GET',
-    }).then(waitResponse);
+async function selectProject(client: ReturnType<typeof bbclient>, query: Pick<Query, 'workspace'>) {
+    const { values } = await waitResponse<{
+        values: {
+            key: string;
+            uuid: string;
+        }[];
+    }>(client(`/workspaces/${query.workspace}/projects?fields=values.key,values.uuid`, { method: 'GET' }));
 
     const key = await question('Select a project (press TAB for hint): ', {
-        choices: Array.from(projectValues).map((v) => get(v, 'key', '-')),
+        choices: Array.from(values).map((v) => get(v, 'key', '-')),
     });
 
-    const matchKey = projectValues.find((item: any) => isEqual(key, item.key));
+    const matchKey = values.find((item) => isEqual(key, item.key));
 
     return get(matchKey, 'uuid', '');
 }
 
-async function getAllRepoSlug(client: ReturnType<typeof bbclient>, query: { workspace: string; project: string }) {
-    const { values: repoValues } = await client(
-        `/repositories/${query.workspace}?fields=values.full_name,values.uuid`,
-        {
-            method: 'GET',
+type GetRepoResult = {
+    next?: string;
+    size: number;
+    values: {
+        uuid: string;
+        slug: string;
+    }[];
+};
+
+async function* getRepoGenerator(client: ReturnType<typeof bbclient>, url: string): AsyncGenerator<GetRepoResult> {
+    let result = await waitResponse<GetRepoResult>(client(url, { method: 'GET' }));
+    yield result;
+
+    while (result.next) {
+        result = await waitResponse<GetRepoResult>(
+            client(result.next.replace(BITBUCKET_BASE_URL, ''), { method: 'GET' }),
+        );
+        yield result;
+    }
+}
+
+async function queryAllRepoGenerator(
+    client: ReturnType<typeof bbclient>,
+    query: Pick<Query, 'workspace' | 'project_key'>,
+) {
+    const q = encodeURI(`project.key="${query.project_key}"`);
+    const url = `/repositories/${query.workspace}?q=${q}&fields=values.slug,values.uuid,next`;
+
+    // fetch repo util next url is null
+    from(getRepoGenerator(client, url)).subscribe({
+        next: (result) => {
+            log([JSON.stringify(result.values, null, 2)]);
         },
-    ).then(waitResponse);
-
-    const repos = Array.from(repoValues).map((v) => get(v, 'full_name', '-'));
-
-    log(repos);
+    });
 }
 
 export async function getEnvFromBitbucketPipelineVariable() {
@@ -76,8 +113,6 @@ export async function getEnvFromBitbucketPipelineVariable() {
     const client = bbclient(username, apptoken);
     const workspace = argvWorkspace ?? (await selectWorkspace(client));
     const project = argvProject ?? (await selectProject(client, workspace));
-    await getAllRepoSlug(client, { workspace, project });
-    // const { values } = await client('/workspaces?fields=values.slug', { method: 'GET' }).then(waitResponse);
 
-    // log([workspace, project]);
+    await queryAllRepoGenerator(client, { workspace, project_key: project });
 }
